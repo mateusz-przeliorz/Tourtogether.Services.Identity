bbe1a519f5f60590683130428a748e8b1ee50660
Mateusz Przeliorz <mateuszprzeliorz@outlook.com>
2020-09-09 00:48:13 +0200

    Change ports

--- scripts/dotnet-run.sh
+++ scripts/dotnet-run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export ASPNETCORE_ENVIRONMENT=local
 cd src/Tourtogether.Services.Identity.Api
-dotnet run --no-restore
+dotnet watch run --no-restore
--- src/Tourtogether.Services.Identity.Api/Startup.cs
+++ src/Tourtogether.Services.Identity.Api/Startup.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -37,7 +38,10 @@ namespace Tourtogether.Services.Identity.Api
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapControllers();
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("Hello World!");
+                });
             });
         }
     }
